6a74e918147bc76d1116f2696978fe52b2c01b28
epicbrownie <epicbrownie@gmail.com>
2019-02-09 17:09:06 -0600

    feat(filters): add convenience filter extensions

---
+++ Turner.Infrastructure.Crud/Configuration/Builders/FilterExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq.Expressions;
+using Turner.Infrastructure.Crud.Configuration.Builders;
+
+// ReSharper disable once CheckNamespace
+namespace Turner.Infrastructure.Crud.Configuration
+{
+    public static class FilterExtensions
+    {
+        public static CrudRequestEntityConfigBuilder<TRequest, TEntity> FilterOn<TRequest, TEntity>(
+            this CrudRequestEntityConfigBuilder<TRequest, TEntity> config,
+            Func<TRequest, Expression<Func<TEntity, bool>>> filterFunc)
+            where TEntity : class
+        {
+            return config.FilterWith(builder => builder.FilterOn(filterFunc));
+        }
+
+        public static CrudBulkRequestEntityConfigBuilder<TRequest, TItem, TEntity> FilterOn<TRequest, TItem, TEntity>(
+            this CrudBulkRequestEntityConfigBuilder<TRequest, TItem, TEntity> config,
+            Func<TRequest, Expression<Func<TEntity, bool>>> filterFunc)
+            where TEntity : class
+        {
+            return config.FilterWith(builder => builder.FilterOn(filterFunc));
+        }
+
+        public static CrudRequestEntityConfigBuilder<TRequest, TEntity> FilterOn<TRequest, TEntity>(
+            this CrudRequestEntityConfigBuilder<TRequest, TEntity> config,
+            Expression<Func<TRequest, TEntity, bool>> filterFunc)
+            where TEntity : class
+        {
+            return config.FilterWith(builder => builder.FilterOn(filterFunc));
+        }
+
+        public static CrudBulkRequestEntityConfigBuilder<TRequest, TItem, TEntity> FilterOn<TRequest, TItem, TEntity>(
+            this CrudBulkRequestEntityConfigBuilder<TRequest, TItem, TEntity> config,
+            Expression<Func<TRequest, TEntity, bool>> filterFunc)
+            where TEntity : class
+        {
+            return config.FilterWith(builder => builder.FilterOn(filterFunc));
+        }
+
+        public static CrudRequestEntityConfigBuilder<TRequest, TEntity> FilterOn<TRequest, TEntity>(
+            this CrudRequestEntityConfigBuilder<TRequest, TEntity> config,
+            Expression<Func<TEntity, bool>> filterFunc)
+            where TEntity : class
+        {
+            return config.FilterWith(builder => builder.FilterOn(filterFunc));
+        }
+
+        public static CrudBulkRequestEntityConfigBuilder<TRequest, TItem, TEntity> FilterOn<TRequest, TItem, TEntity>(
+            this CrudBulkRequestEntityConfigBuilder<TRequest, TItem, TEntity> config,
+            Expression<Func<TEntity, bool>> filterFunc)
+            where TEntity : class
+        {
+            return config.FilterWith(builder => builder.FilterOn(filterFunc));
+        }
+    }
+}
