146feffa558fc435e956bbc1a0985fcb230cd13e
epicbrownie <epicbrownie@gmail.com>
2019-02-27 22:10:26 -0600

    feat(nuget packages): add Turner.Infrastructure.Crud.All project and add project page to nuget packages

---
+++ Turner.Infrastructure.Crud.All/CommonInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using SimpleInjector;
+using Turner.Infrastructure.Crud.EntityFrameworkCore;
+using Turner.Infrastructure.Crud.FluentValidation;
+
+namespace Turner.Infrastructure.Crud.All
+{
+    public static partial class Crud
+    {
+        public static void Initialize(Container container, Assembly[] assemblies = null)
+        {
+            Infrastructure.Crud.Crud
+                .CreateInitializer(container)
+                .WithAssemblies(assemblies)
+                .UseEntityFramework()
+                .UseFluentValidation()
+                .Initialize();
+        }
+    }
+}
